0ab29621a02e0e9c6b6b3cc521b3491b99b34851
aniket <escaperealityproject@gmail.com>
2018-07-04 17:42:53 +0530

    Styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,18 +1,21 @@
-$("ul").on("click","li",function() {
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
-$("ul").on("click","span",function(event) {
-  $(this).parent().fadeOut(500,function(){
-      $(this).remove();
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
   });
   event.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(event){
-  if(event.which===13)
-  {
-    $("ul").append("<li><span>X</span> "+ $(this).val()+"</li>");
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + $(this).val() + "</li>");
     $(this).val("");
   }
+})
+
+$(".fa-plus-circle").click(function() {
+  $("input").fadeToggle();
 })
